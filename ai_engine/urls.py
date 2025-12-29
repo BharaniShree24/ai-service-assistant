d91a486d013013fc67d5fa23a12066057bcfa7c1
Bharani Shree R <bharani.shree@b2e.in>
2025-12-29 16:52:57 +0530

    Add files via upload

--- ai_engine/urls.py
+++ ai_engine/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import chat_ui, chat_api
 
 urlpatterns = [
-    path("", chat_ui, name="chat_ui"),
+    path("ai_assistant/", chat_ui, name="chat_ui"),
     path("api/chat/", chat_api, name="chat_api"),
 ]
